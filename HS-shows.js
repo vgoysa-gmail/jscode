05c40fc77f6fec599b798a9cb38f4af71a73bfd0
Василий Гойса <vgoysa@gmail.com>
2019-04-13 19:09:36 +0300

    updated change watching

--- HS-shows.js
+++ HS-shows.js
@@ -29,9 +29,63 @@ var goodlist = [
   "Fairy Tail Final Season - ",
   "Gegege no Kitarou (2018) - "
 ];
+makeTag = t => document.createElement(t);
+add = (b,h) => b.insertAdjacentElement("afterend", h);
+function makeText(tag,text){
+  let tt=makeTag(tag);
+  tt.appendChild(document.createTextNode(text));
+  return tt;
+}
+function makeBtn(n,oncl,hr){
+  let b=makeText("button",n);
+  b.addEventListener("click",oncl);
+  b.href=hr;
+  b.className="badge";
+  return b;
+}
+const Cookie = {
+  get: name => {
+      let c = document.cookie.match(`(?:(?:^|.*; *)${name} *= *([^;]*).*$)|^.*$`)[1]
+      if (c) return decodeURIComponent(c)
+      }
+  , set: (name, value, opts = {}) => {
+      if (opts.days) {
+          opts['max-age'] = opts.days * 60 * 60 * 24;
+          delete opts.days
+          }
+      opts = Object.entries(opts).reduce((str, [k, v]) => `${str}; ${k}=${v}`, '')
+      document.cookie = name + '=' + encodeURIComponent(value) + opts
+      }
+  , delete: function(name, opts){
+      let opts1 = opts;
+      opts1["max-age"] = -1;
+      Cookie.set(name, '', opts1)
+    }
+  , getJSON: name => JSON.parse(Cookie.get(name)||"{}")
+  , getList: name => JSON.parse(Cookie.get(name)||"[]")
+  , setJSON: (name, value, opts) => Cookie.set(name, JSON.stringify(value), opts)
+}
+prep = (name) => name.replace(/[^a-zA-Z0-9]/g,"");
+function record_click(evt) {
+  if (this.hasAttribute("episodet")) {
+    // alert(this.episodet+this.episoden);
+    let done = Cookie.getList(prep(this.episodet));
+    // alert(typeof(done));
+    if (!done.includes(this.episoden)) {
+      done.push(this.episoden);
+      // alert(done);
+      Cookie.setJSON(
+        prep(this.episodet),
+        done,
+        {path:"/",days:30}
+      );
+    }
+  }
+  // evt.preventDefault();
+}
 function load_list_async(listName) {
-    var xhr = new XMLHttpRequest();
-		var list_data;
+    let xhr = new XMLHttpRequest();
+		let list_data;
     xhr.withCredentials = true;
     xhr.open("GET", "https://dkvm/api/list/"+listName);
     xhr.responseType = 'json';
@@ -54,10 +108,15 @@ function load_list_async(listName) {
         }
         fullset = blacklist.concat(goodlist, probelist);
         updates.push(listName);
-        console.log(listName+" done;");
-        if (updates.length==3) {
+        // console.log(listName+" done;");
+        if (updates.includes("good")
+          &&updates.includes("black")
+          &&updates.includes("trial")) {
           updates=[];
-          hs_load({});
+          hs_load({
+            src:"load_list_async",
+            href:document.documentURI,
+            dom:document.domain});
         }
       }
     });
@@ -80,17 +139,27 @@ function update_lists() {
   load_list_async("trial");
   console.log("updated",fullset);
 }
-update_lists();
 function record(evt) {
   if (this.href && confirm(this.href+" "+this.textContent)) {
-		var data = JSON.stringify({
+    switch (this.textContent) {
+      case "+":
+        goodlist.push(this.href);
+        break;
+      case "+":
+        probelist.push(this.href);
+        break;
+      case "-":
+        blacklist.push(this.href);
+        break;
+    }
+		let data = JSON.stringify({
       "shows": [
         this.href
       ],
       "action": this.textContent
     });
-    var cl = this.classList;
-    var xhr = new XMLHttpRequest();
+    let cl = this.classList;
+    let xhr = new XMLHttpRequest();
     xhr.withCredentials = true;
 
     xhr.addEventListener("readystatechange", function() {
@@ -110,7 +179,7 @@ function record(evt) {
 
 function btnclick(event) {
   if (this.href) {
-    var data = JSON.stringify({
+    let data = JSON.stringify({
       "urls": [
         this.href
       ],
@@ -121,8 +190,8 @@ function btnclick(event) {
         "HS"
       ]
     });
-    var cl = this.classList;
-    var xhr = new XMLHttpRequest();
+    let cl = this.classList;
+    let xhr = new XMLHttpRequest();
     xhr.withCredentials = true;
 
     xhr.addEventListener("readystatechange", function() {
@@ -140,28 +209,12 @@ function btnclick(event) {
   }
   event.preventDefault();
 }
-update_lists();
 function hs_load(evt) {
-  //console.log(evt.explicitOriginalTarget.baseURI,evt.originalTarget.baseURI,evt);
-  //if (!evt.originalTarget.baseURI.includes("https://horriblesubs.info/")) {
-  //  return false;
-  //}
-  var d = document;
-  var s = d.querySelectorAll(".hs-magnet-link > a");
-  function add(b,h){return b.insertAdjacentElement("afterend", h);}
-  function makeTag(t){return d.createElement(t);}
-  function makeBtn(n,oncl,hr){var b=makeText("button",n);
-    b.addEventListener("click",oncl);
-    b.href=hr;
-    b.className="badge";
-    return b;
-  }
-  function makeText(tag,text){
-    tt=makeTag(tag);
-    tt.appendChild(d.createTextNode(text));
-    return tt;
-  }
+  console.log(evt);
+  let d = document;
+  let s = d.querySelectorAll(".hs-magnet-link > a");
   if (s.length > 0) {
+    // specific show page
     a = "";
     floodLogin();
     for (i = 0; i < s.length; i++) {
@@ -173,41 +226,86 @@ function hs_load(evt) {
       }
     }
   } else {
-    var btn = d.querySelector(".latest-show-more");
+    // show list page
+    let btn = d.querySelector(".latest-show-more");
     if (btn && !btn.hasAttribute("parsed")) {
       btn.setAttribute("parsed", true);
       btn.classList.add("good");
-      btn.addEventListener("mouseup", hs_load);
+      // btn.addEventListener("change",hs_load);
+      // btn.addEventListener("mouseup", hs_load);
     }
-    var s = d.querySelectorAll(".latest-releases > ul > li > a");
-    for (var p of s) {
-      if (!p.hasAttribute("parsed")) {
-        for (var t of p.childNodes) {
-          if (t.nodeType == 3) {
-            console.log(t.textContent);
-            if (fullset.includes(t.nodeValue)) {
-              p.setAttribute("parsed", true);
-              p.classList.add(triage[t.nodeValue]);
-            } else if (!p.hasAttribute("undecided")) {
-              p.setAttribute("undecided",true)
-              p.appendChild(makeBtn("-",record,t.nodeValue));
-              p.appendChild(makeBtn("?",record,t.nodeValue));
-              p.appendChild(makeBtn("+",record,t.nodeValue));
-            }
-          }
-        }
-      }
+    let s = d.querySelectorAll(".latest-releases > ul > li > a");
+    // console.log(s);
+    for (let p of s) {
+      lookup_show(p);
+    }
+    if (!showList) {
+      showList = document.querySelector(".latest-releases");
+      if (showList) {observer.observe(showList, config);}
     }
   }
 }
-window.addEventListener("loadend", hs_load, false);
+
+function wait_shows (mutationsList, observer) {
+  for(let mutation of mutationsList) {
+      // console.log(mutation);
+      if (mutation.type == 'childList' && mutation.addedNodes) {
+        for (let node of mutation.addedNodes) {
+          if (node.nodeName=="UL") {
+            for (let a of node.getElementsByTagName("A")){
+              lookup_show(a);
+            }
+          }
+          // console.log(node, node.childNodes);
+        }
+      }
+  }
+};
+function lookup_show(p) {
+  if (!p.hasAttribute("parsed")) {
+    p.addEventListener("click",record_click,false);
+    let episode = {};
+    for (let t of p.childNodes) {
+      // console.log(t.nodeName,t.nodeType,t.nodeValue);
+      if (t.nodeName == "STRONG") {
+        episode.number = t.childNodes[0].nodeValue;
+      }
+      if (t.nodeType == 3) {
+        episode.name = t.nodeValue;
+        console.log(t.textContent);
+      }
+    }
+    // console.log(episode);
+    done = Cookie.getList(prep(episode.name));
+    // console.log(done);
+    if (done.includes(episode.number)) {
+      p.classList.add("done");
+    }
+    p.setAttribute("episodet",episode.name);
+    p.setAttribute("episoden",episode.number);
+    p.episodet=episode.name;
+    p.episoden=episode.number;
+    if (fullset.includes(episode.name)) {
+      p.setAttribute("parsed", true);
+      p.parsed = true;
+      p.classList.add(triage[episode.name]);
+    } else if (!p.hasAttribute("undecided")) {
+      p.setAttribute("undecided",true);
+      p.undecided = true;
+      ["-","?","+"].forEach(
+        (x)=>p.appendChild(makeBtn(x,record,episode.name))
+      );
+    }
+  }
+
+}
 function floodLogin() {
-  var data = {
+  let data = {
     "username": "vgoissa",
     "password": "Cisco123"
   };
 
-  var xhr = new XMLHttpRequest();
+  let xhr = new XMLHttpRequest();
   xhr.withCredentials = true;
 
   xhr.addEventListener("readystatechange", function() {
@@ -222,3 +320,40 @@ function floodLogin() {
 
   xhr.send(data);
 }
+
+update_lists();
+var config = { attributes: false, childList: true, subtree: true };
+var observer = new MutationObserver(wait_shows);
+var showList = document.querySelector(".latest-releases");
+if (showList) {observer.observe(showList, config);}
+window.addEventListener("DOMContentLoaded", hs_load, false);
+
+/*
+var targetNode = document.getElementById('some-id');
+
+// Options for the observer (which mutations to observe)
+var config = { attributes: false, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+var callback = function(mutationsList, observer) {
+    for(var mutation of mutationsList) {
+        console.log(mutation);
+        // if (mutation.type == 'childList') {
+        //     console.log('A child node has been added or removed.');
+        // }
+        // else if (mutation.type == 'attributes') {
+        //     console.log('The ' + mutation.attributeName + ' attribute was modified.');
+        // }
+    }
+};
+
+// Create an observer instance linked to the callback function
+var observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
+
+// Later, you can stop observing
+observer.disconnect();
+
+*/
